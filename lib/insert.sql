847a85efe5c32bb9c02854f49b24121d145309ce
Jack Goode <jack.w.goode@icloud.com>
2020-07-24 00:38:57 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,57 @@
+INSERT INTO bears
+(name, age, gender, color, temperament, alive)
+VALUES
+('Mr. Chocolate',8, 'Male', 'brown', 'playful', 1);
+INSERT INTO bears
+(name, age, gender, color, temperament, alive)
+VALUES
+('Rowdy',8, 'Male', 'red', 'playful', 1);
+INSERT INTO bears
+(name, age, gender, color, temperament, alive)
+VALUES
+('Tabitha',9, 'Female', 'brown', 'playful', 1);
+INSERT INTO bears
+(name, age, gender, color, temperament, alive)
+VALUES
+('Sergeant Brown',6, 'Female', 'brown', 'aggressive', 1);
+INSERT INTO bears
+(name, age, gender, color, temperament, alive)
+VALUES
+('Melissa', 5, 'Female', 'black', 'shy', 1);
+INSERT INTO bears
+(name, age, gender, color, temperament, alive)
+VALUES
+('Grinch',9, 'Male', 'green', 'grumpy', 1);
+INSERT INTO bears
+(name, age, gender, color, temperament, alive)
+VALUES
+('Wendy',9, 'Female', 'brown', 'affectionate', 1);
+INSERT INTO bears
+(name, age, gender, color, temperament, alive)
+VALUES
+(NULL, 20, 'Male', 'black', 'aggressive', 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- name   |text   |
+-- |age    |integer|
+-- |gender |text   |(The choices could be "Male", "Female", "Other", or "Prefer not to say")|
+-- |color  |text   |
+-- |temperament|text|
+-- |alive  |boolean
